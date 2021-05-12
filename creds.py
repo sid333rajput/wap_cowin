e889a15c44eb725941708671b820a16f9b0e68a0
Siddhant Singh <sid333rajput@gmail.com>
2021-05-12 14:44:10 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,7 @@
+ACCOUNT_SID = 'AC6d8d66594f9cdcffb629166b49f2ca4c'  # put your Twilio Account sid here
+AUTH_TOKEN = '8d5b9f0bf3cbf4aa76bb520870e5f527'  # put your Twilio Auth token here
+TWILIO_ALLOTED_NUMBER = '+16789169292'  # put your Twilio alloted number here
+# Header if necessary otherwise Govt. API thinks request is coming from bot , not from actual browser and block your request.
+HEADER = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+YOUR_PHONE_NO = '+918425941363'  # put your phone no with country code here for error updates (eg : +919876543210)
